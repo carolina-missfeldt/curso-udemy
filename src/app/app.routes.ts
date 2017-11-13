48586534c8ab42f99eb6eb316cc3ab8e0235f00b
carolinamiss <carolina.missfeldt@gmail.com>
2017-11-13 10:09:05 -0200

    aula(46)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
+import { DetalheRestaurantComponent } from 'app/detalhe-restaurant/detalhe-restaurant.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent },
     {path: 'about', component: AboutComponent},
-    {path: 'restaurantes', component: RestaurantsComponent}
+    {path: 'restaurantes', component: RestaurantsComponent},
+    {path: 'restaurantes/:id', component: DetalheRestaurantComponent},
 
 ]
