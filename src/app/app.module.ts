d96b1b918535c060994a5f3053fcbb33f4159615
carolinamiss <carolina.missfeldt@gmail.com>
2017-11-17 12:25:45 -0200

    carrinho de compras (aula-65)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms'
+
+import { ROUTES } from 'app/app.routes';
 import { RestaurantService } from 'app/restaurants/restaurant.service';
 import { CarrinhoDeComprasService } from 'app/detalhe-restaurant/carrinho-de-compras/carrinho-de-compras.service';
-import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
-import { ROUTES } from 'app/app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestauranteComponent } from './restaurants/restaurante/restaurante.component';
 import { DetalheRestaurantComponent } from './detalhe-restaurant/detalhe-restaurant.component';
@@ -22,11 +22,7 @@ import { ReviewsComponent } from 'app/detalhe-restaurant/reviews/reviews.compone
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from 'app/shared/input/input.component';
 import { RadioComponent } from 'app/shared/radio/radio.component';
-
-
-
-
-
+import { OrderItensComponent } from 'app/order/order-itens/order-itens.component';
 
 
 @NgModule({
@@ -44,7 +40,8 @@ import { RadioComponent } from 'app/shared/radio/radio.component';
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItensComponent
   ],
   imports: [
     BrowserModule,
